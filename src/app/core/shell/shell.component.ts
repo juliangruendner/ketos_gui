18b1a623837e4b5035808ece6a19bc001c0cc3fb
philipp <philsippl@gmail.com>
2017-11-23 17:24:05 +0100

    side menu

--- src/app/core/shell/shell.component.ts
+++ src/app/core/shell/shell.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShellComponent implements OnInit {
 
