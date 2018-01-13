fd1bc773cc56c201ded45d2ef99ac7f88143ac6e
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2018-01-13 19:30:14 +0100

    features can now be deleted and changed

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -9,13 +9,15 @@ import { Feature } from '../models/features.model';
 })
 export class FeaturesComponent implements OnInit {
 
-  features: Feature[] = []
+  features: Feature[] = [];
+  feature: Feature = new Feature();
 
-  create_resource: string;
-  create_parameter_name: string;
-  create_value: string;
-  create_name: string;
-  create_description: string;
+  is_form_create: boolean;
+  form_resource: string;
+  form_parameter_name: string;
+  form_value: string;
+  form_name: string;
+  form_description: string;
 
   constructor(private featuresService: FeaturesService) { }
 
@@ -25,21 +27,49 @@ export class FeaturesComponent implements OnInit {
     });
   }
 
-  clearCreateInput() {
-    this.create_resource = this.create_parameter_name = this.create_value = this.create_name = this.create_description = null;
+  clearFormInput() {
+    this.form_resource = this.form_parameter_name = this.form_value = this.form_name = this.form_description = null;
+  }
+
+  setFormInput(feature: Feature) {
+    this.feature = feature;
+    this.form_resource = feature.resource;
+    this.form_parameter_name = feature.parameter_name;
+    this.form_value = feature.value;
+    this.form_name = feature.name;
+    this.form_description = feature.description;
   }
 
   create() {
     var feature = new Feature();
-    feature.resource = this.create_resource;
-    feature.parameter_name = this.create_parameter_name;
-    feature.value = this.create_value;
-    feature.name = this.create_name;
-    feature.description = this.create_description;
-    this.featuresService.post(feature).subscribe(f => { 
+    feature.resource = this.form_resource;
+    feature.parameter_name = this.form_parameter_name;
+    feature.value = this.form_value;
+    feature.name = this.form_name;
+    feature.description = this.form_description;
+
+    this.featuresService.post(feature).subscribe(f => {
       this.features.push(f);
     });
-    this.clearCreateInput();
+    this.clearFormInput();
+  }
+
+  edit() {
+    this.feature.resource = this.form_resource;
+    this.feature.parameter_name = this.form_parameter_name;
+    this.feature.value = this.form_value;
+    this.feature.name = this.form_name;
+    this.feature.description = this.form_description;
+
+    this.featuresService.putSingle(this.feature.id, this.feature).subscribe((retF) => {
+      this.features[this.features.findIndex(f => retF.id === f.id)] = retF;
+    });
+  }
+
+  delete() {
+    this.featuresService.delete(this.feature.id).subscribe((id) => {
+      this.features.splice(this.features.findIndex(feature => id.id === feature.id), 1);
+    });
   }
 
 }
